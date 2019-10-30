4bbc83a9ebc6a24aab30274fa44c1d87626c41f5
Akib <akib3366@gmail.com>
2019-10-30 13:22:41 +0600

    Admin and customer Models added

---
+++ models/Customer.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+
+const CustomerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  address: {
+    type: String,
+  },
+});
+
+module.exports = Customer = mongoose.model(
+  'customers',
+  CustomerSchema,
+);
